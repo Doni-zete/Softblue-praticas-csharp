1bced694175f008a0e14b4eb64662b21be1e94d6
Doni-zete <donizete112@gmail.com>
2021-08-10 18:49:50 -0300

    equals

--- Equals/Program.cs
+++ Equals/Program.cs
@@ -16,7 +16,7 @@ namespace Equals
             Console.WriteLine(i1.Equals(i2));
 
             Aluno a1 = new Aluno("Pedro");
-            Aluno a2 = new Aluno("Pedro");
+            Aluno a2 = a1; //Aluno("Pedro");
             Console.WriteLine(a1.Equals(a2));
 
         }
@@ -25,8 +25,23 @@ namespace Equals
 public class Aluno
 {
     public string Nome { get; set; }
-    public Aluno (string nome)
+    public Aluno(string nome)
     {
         this.Nome = nome;
     }
+
+    public override bool Equals(object obj)
+    {
+        Aluno aluno = obj as Aluno;
+        if (aluno == null)
+        {
+            throw new ArgumentException("Objeto deve ser um Aluno");
+
+        }
+
+        return this.Nome.Equals(aluno.Nome);
+    }
+
 }
+
+
