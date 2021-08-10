86ffafdd0f1b6994c4d42c9f0c5c9475a3a1c1e9
Doni-zete <donizete112@gmail.com>
2021-08-10 13:49:17 -0300

    softblue

---
+++ Listas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lista
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> nomes = new List<string>();
+            nomes.Add("José");
+            nomes.Add("Maria");
+            nomes.Add("Pedro");
+            nomes.Insert(1, "Paula");
+
+
+            foreach (string nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+
+            for (int i = 0; i < nomes.Count; i++)
+            {
+                string nome = nomes[i];
+                Console.WriteLine(nome);
+            }
+
+            bool b = nomes.Contains("José");
+            Console.WriteLine(b);
+        }
+    }
+}
