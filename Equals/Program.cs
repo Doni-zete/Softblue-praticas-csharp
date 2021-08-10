19d649c40dd7f659a72eedd86a51af049b14320f
Doni-zete <donizete112@gmail.com>
2021-08-10 14:36:32 -0300

    equals

--- Equals/Program.cs
+++ Equals/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Equals
 {
@@ -10,6 +7,26 @@ namespace Equals
     {
         static void Main(string[] args)
         {
+            string s1 = "abc";
+            string s2 = "abc";
+
+            Console.WriteLine(s1.Equals(s2));
+            int i1 = 10;
+            int i2 = 10;
+            Console.WriteLine(i1.Equals(i2));
+
+            Aluno a1 = new Aluno("Pedro");
+            Aluno a2 = new Aluno("Pedro");
+            Console.WriteLine(a1.Equals(a2));
+
         }
+    }
+}
+public class Aluno
+{
+    public string Nome { get; set; }
+    public Aluno (string nome)
+    {
+        this.Nome = nome;
     }
 }
