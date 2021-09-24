10bc74ba75483c471bd85ccadd9f6de7c487052f
Doni-zete <donizete112@gmail.com>
2021-09-24 20:30:55 -0300

    HashSet

--- Conjuntos/Program.cs
+++ Conjuntos/Program.cs
@@ -9,67 +9,68 @@ namespace Conjuntos
         {
             HashSet<string> dias = new HashSet<string>();
 
-            //        dias.Add("Segunda");
-            //        dias.Add("Terça");
-            //        dias.Add("Quarta");
-            //        dias.Add("Quinta");
-            //        dias.Add("Sexta");
-            //        dias.Remove("Terça");
+            dias.Add("Segunda");
+            dias.Add("Terça");
+            dias.Add("Quarta");
+            dias.Add("Quinta");
+            dias.Add("Sexta");
+            dias.Remove("Terça");
 
-            //        foreach (String dia in dias)
-            //        {
-            //            Console.WriteLine(dia); ;
-            //        }
-
-            //    }
-            //}
-
-            HashSet<Conta> contas = new HashSet<Conta>();
-
-            contas.Add(new Conta(3456));
-            contas.Add(new Conta(5681));
-            contas.Add(new Conta(2121));
-            contas.Add(new Conta(1379));
-            contas.Add(new Conta(1379));
-
-            foreach (Conta conta in contas)
+            foreach (String dia in dias)
             {
-                Console.WriteLine(conta.Numero);
+                Console.WriteLine(dia); ;
             }
-
 
         }
-
-        public class Conta:IEquatable<Conta>
-        {
-            public int Numero { get; set; }
-            public Conta(int numero)
-            {
-                this.Numero = numero;
-            }
-
-            public override bool Equals(object obj)
-            {
-                
-                return Equals (obj as Conta);
-            }
-
-            public bool Equals(Conta other)
-            {
-                if(other == null)
-                {
-                    return false;
-                }
-                return this.Numero == other.Numero;
-            }
-
-            public override int GetHashCode()
-            {
-                int hash = 27;
-                hash = (13 * hash) + Numero.GetHashCode();
-  
-              return hash;
-            }
-        }
     }
-    }
+}
+
+    //        HashSet<Conta> contas = new HashSet<Conta>();
+
+//        contas.Add(new Conta(3456));
+//        contas.Add(new Conta(5681));
+//        contas.Add(new Conta(2121));
+//        contas.Add(new Conta(1379));
+//        contas.Add(new Conta(1379));
+
+//        foreach (Conta conta in contas)
+//        {
+//            Console.WriteLine(conta.Numero);
+//        }
+
+
+//    }
+//}
+
+//    public class Conta:IEquatable<Conta>
+//    {
+//        public int Numero { get; set; }
+//        public Conta(int numero)
+//        {
+//            this.Numero = numero;
+//        }
+
+//        public override bool Equals(object obj)
+//        {
+
+//            return Equals (obj as Conta);
+//        }
+
+//        public bool Equals(Conta other)
+//        {
+//            if(other == null)
+//            {
+//                return false;
+//            }
+//            return this.Numero == other.Numero;
+//        }
+
+//        public override int GetHashCode()
+//        {
+//            int hash = 27;
+//            hash = (13 * hash) + Numero.GetHashCode();
+
+//        }
+//    }
+//}
+//}
