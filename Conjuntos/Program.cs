d95524cdb6d1c683bd9245d099ec924a1a8cc9cf
Doni-zete <donizete112@gmail.com>
2021-09-24 17:28:32 -0300

    HastSet

---
+++ Conjuntos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Conjuntos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> dias = new HashSet<string>();
+            
+            dias.Add("Segunda");
+            dias.Add("Terça");
+            dias.Add("Quarta");
+            dias.Add("Quinta");
+            dias.Add("Sexta");
+            dias.Remove("Terça");
+
+            foreach(String dia in dias)
+            {
+                Console.WriteLine(dia); ;
+            }
+
+        }
+    }
+}
