e35f6f630c9fb672850545b00657c48d6160b195
Doni-zete <donizete112@gmail.com>
2021-11-22 23:48:56 -0300

    filaspilhas

---
+++ FilasPilhas/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace FilasPilhas
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Queue<char> queue = new Queue<char>();
+            while (true)
+            {
+                Console.WriteLine("Elemento: ");
+                string s;
+                if (String.IsNullOrWhiteSpace(s))
+                {
+                    break;
+                }
+                char c = s[0];
+                queue.Enqueue(c);
+            }
+            while (true)
+            {
+                char c = queue.Dequeue();
+                Console.WriteLine("=> "+c);
+                if (queue.Count == 0)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
