5f172336a80ad309118b1157f44023d14b8930b2
Doni-zete <donizete112@gmail.com>
2021-12-13 23:05:19 -0300

    filehelper

--- Dio-praticas-c#/EXEMPLOS/helper/FileHelper.cs
+++ Dio-praticas-c#/EXEMPLOS/helper/FileHelper.cs
@@ -6,7 +6,7 @@ namespace EXEMPLOS.helper
     {
         public void ListarDiretorios(string caminho)
         {
-            var retornoCaminho= Directory.GetDirectories(caminho);
+            var retornoCaminho= Directory.GetDirectories(caminho,"*",SearchOption.AllDirectories);
             foreach(var retorno in retornoCaminho)
             {
                 System.Console.WriteLine(retorno);
