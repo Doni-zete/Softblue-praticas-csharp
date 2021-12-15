21b0bf1e57d99dac9556a222bad5790f468d80c8
Doni-zete <donizete112@gmail.com>
2021-12-15 15:24:15 -0300

    stream

--- Dio-praticas-c#/EXEMPLOS/Program.cs
+++ Dio-praticas-c#/EXEMPLOS/Program.cs
@@ -11,17 +11,19 @@ namespace Exemplos
   {
     static void Main(string[] args)
     {
-      var caminho = "E:\\FATEC";
-      var caminhoArquivo = Path.Combine(caminho,"arquivo-texte.txt");
-      var listaString = new List<string> {"Linha1", "Linha2", "Linha3"};
+      var caminho = "E:\\FATEC\\CURSOS\\GITHUB\\PROJETOS-C#\\Dio-progamando\\Softblue-praticas-csharp\\Dio-praticas-c#\\EXEMPLOS";
+      
+      
+      var caminhoArquivo = Path.Combine(caminho,"arquivo-texte1.txt");
+      // var listaString = new List<string> {"Linha1", "Linha2", "Linha3"};
 
       FileHelper helper = new FileHelper();
       // helper.ListarDiretorios(caminho);
       // helper.ListarArquivosDiretorio(caminho);
       // helper.CriarDiretorio(Path.Combine(caminho,"Pasta"));
       // helper.ApagarDiretorio(caminhoPathCombine)
-    // helper.CriarArquivoTexto(caminhoArquivo,"Olá! Teste de escrito de arquivo");
-    helper.CriarArquivoTextoStream(caminhoArquivo,listaString);
+    helper.CriarArquivoTexto(caminhoArquivo,"Olá! Teste de escrito de arquivo");
+    // helper.CriarArquivoTextoStream(caminhoArquivo,listaString);
 
       // ICalculadora calc = new Calculadora();
       // System.Console.WriteLine(calc.Dividir(10,2));
