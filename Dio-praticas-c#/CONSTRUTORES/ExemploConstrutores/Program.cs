1350d0f55e4be9320c98c26a93edaa598e2785ae
Doni-zete <donizete112@gmail.com>
2021-12-15 23:08:34 -0300

    contrutores

---
+++ Dio-praticas-c#/CONSTRUTORES/ExemploConstrutores/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ExemploConstrutores.models;
+
+namespace ExemploConstrutores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa  p1 = new Pessoa();
+            p1.Apresentar();
+        }
+    }
+}
