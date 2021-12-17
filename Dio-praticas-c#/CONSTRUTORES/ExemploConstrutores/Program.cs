ab630c3f604feb5efbf4a60fef8a087d15005df4
Doni-zete <donizete112@gmail.com>
2021-12-16 23:13:47 -0300

    delegate

--- Dio-praticas-c#/CONSTRUTORES/ExemploConstrutores/Program.cs
+++ Dio-praticas-c#/CONSTRUTORES/ExemploConstrutores/Program.cs
@@ -5,14 +5,24 @@ namespace ExemploConstrutores
 {
     class Program
     {
+        public delegate void Operacao (int x, int y);
         static void Main(string[] args)
-        {
-            Data data =new Data();
-            // data.Setmes(20);
 
-            data.Mes =20;
-            System.Console.WriteLine(data.Mes);
-            data.ApresentarMes();
+        {
+            // Operacao op Calculadora.Somar;
+            Operacao op = new Operacao(Calculadora.Somar);
+            op.Invoke(10,10);
+
+            // const double pi =3.14;
+
+            // System.Console.WriteLine(pi);
+           
+            // Data data =new Data();
+            // // data.Setmes(20);
+
+            // data.Mes =20;
+            // System.Console.WriteLine(data.Mes);
+            // data.ApresentarMes();
             // Log log = Log.GetInstance();
 
             // log.propriedadeLog ="Teste instancia";
