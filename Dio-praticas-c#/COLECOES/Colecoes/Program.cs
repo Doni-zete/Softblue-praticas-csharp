2fdfc4707ef2a5486daccb42a6fe6a7419fdf4f4
Doni-zete <donizete112@gmail.com>
2021-12-20 21:21:58 -0300

    int.parse

--- Dio-praticas-c#/COLECOES/Colecoes/Program.cs
+++ Dio-praticas-c#/COLECOES/Colecoes/Program.cs
@@ -6,27 +6,28 @@ namespace Colecoes
   {
     static void Main(string[] args)
     {
-      int[] arrayInteiros = new int[3];
+      // int[] arrayInteiros = new int[3];
 
-      arrayInteiros[0] = 10;
-      arrayInteiros[1] = 20;
-      arrayInteiros[2] = 40;
-     
+      // arrayInteiros[0] = 10;
+      // arrayInteiros[1] = 20;
+      // arrayInteiros[2] = int.Parse("40");
 
-      System.Console.WriteLine("Percorrendo o array pelo for");
+      // arrayInteiros[3]=30;
 
-      for (int i = 0; i < arrayInteiros.Length; i++)
-      {
-        System.Console.WriteLine(arrayInteiros[i]);
-      }
 
-      System.Console.WriteLine("Percorrendo o array pelo ForEach");
-      foreach (int item in arrayInteiros)
+      // System.Console.WriteLine("Percorrendo o array pelo for");
+      // for (int i = 0; i < arrayInteiros.Length; i++)
+      // {
+      //   System.Console.WriteLine(arrayInteiros[i]);
+      // }
 
-      {
-        System.Console.WriteLine(item);
+      // System.Console.WriteLine("Percorrendo o array pelo ForEach");
+      // foreach (int item in arrayInteiros)
 
-      }
+      // {
+      //   System.Console.WriteLine(item);
+
+      // }
 
     }
   }
