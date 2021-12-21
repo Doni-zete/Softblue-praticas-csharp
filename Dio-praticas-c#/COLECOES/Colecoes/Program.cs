9cbb901cd7ee857393dc55da3b7099c84f519ca6
Doni-zete <donizete112@gmail.com>
2021-12-20 22:14:24 -0300

    OrdenarBurbleSort

--- Dio-praticas-c#/COLECOES/Colecoes/Program.cs
+++ Dio-praticas-c#/COLECOES/Colecoes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Colecoes.Helper;
 
 namespace Colecoes
 {
@@ -6,6 +7,35 @@ namespace Colecoes
   {
     static void Main(string[] args)
     {
+      OperacoesArray op = new OperacoesArray();
+      int[] array = new int[5] { 6, 38, 1, 9, 2 };
+
+      System.Console.WriteLine("Array original:");
+      op.ImprimirArray(array);
+
+      op.OrdenarBurbleSort(ref array);
+      
+      System.Console.WriteLine("Array ordenado:");
+      op.ImprimirArray(array);
+
+      // int[,] matriz = new int[4, 2]
+      // {
+      //     {8,8},
+      //     {10,20},
+      //     {30,50},
+      //     {2,5}
+      // };
+      // for (var i = 0; i < matriz.GetLength(0); i++)
+      // {
+      //   for (var j = 0; j < matriz.GetLength(1); j++)
+      //   {
+      //     System.Console.WriteLine(matriz[i,j]);    
+      //   }
+      // }
+
+
+
+
       // int[] arrayInteiros = new int[3];
 
       // arrayInteiros[0] = 10;
