8fd45ddb971e4f333c5d3735303e6d6274dd8be9
Doni-zete <donizete112@gmail.com>
2021-12-23 20:59:42 -0300

    Dictionary

--- Dio-praticas-c#/COLECOES/Colecoes/Program.cs
+++ Dio-praticas-c#/COLECOES/Colecoes/Program.cs
@@ -9,21 +9,30 @@ namespace Colecoes
 
     static void Main(string[] args)
     {
+      Dictionary<string, string>estados = new Dictionary<string, string>();
+      estados.Add("SP","São Paulo");
+      estados.Add("MG", "Minas Gerais");
+      estados.Add("BA", "Bahia");
 
-      Stack<string> pilhaLivros = new Stack<string>();
-
-      pilhaLivros.Push(".NET");
-      pilhaLivros.Push("DDD");
-      pilhaLivros.Push("Codigo limpo");
-
-      System.Console.WriteLine($"Livros para a leitura: {pilhaLivros.Count}");
-      while(pilhaLivros.Count>0)
+      foreach(KeyValuePair<string, string>item in estados)
       {
-        System.Console.WriteLine($"Proximo livro para a leitura: {pilhaLivros.Peek()}");
-        System.Console.WriteLine($"{pilhaLivros.Pop()} lido com sucesso");
+        System.Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
       }
 
-      System.Console.WriteLine($"Livros para a leitura: {pilhaLivros.Count}");
+      // Stack<string> pilhaLivros = new Stack<string>();
+
+      // pilhaLivros.Push(".NET");
+      // pilhaLivros.Push("DDD");
+      // pilhaLivros.Push("Codigo limpo");
+
+      // System.Console.WriteLine($"Livros para a leitura: {pilhaLivros.Count}");
+      // while(pilhaLivros.Count>0)
+      // {
+      //   System.Console.WriteLine($"Proximo livro para a leitura: {pilhaLivros.Peek()}");
+      //   System.Console.WriteLine($"{pilhaLivros.Pop()} lido com sucesso");
+      // }
+
+      // System.Console.WriteLine($"Livros para a leitura: {pilhaLivros.Count}");
 
 
       // Queue<string> fila = new Queue<string>();
